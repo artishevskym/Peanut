e46a9ee9c36952930ef2975fc78ec05357243628
Martin Artishevsky <artishevsky.m@gmail.com>
2019-10-30 17:41:28 +0000

    Implement steps to pass invalid password test. Create LoginRobot

---
+++ app/src/androidTest/java/com/artishevskym/peanut/test/robots/LoginRobot.kt
@@ -0,0 +1,37 @@
+package com.artishevskym.peanut.test.robots
+
+import com.artishevskym.peanut.R
+import com.schibsted.spain.barista.assertion.BaristaErrorAssertions.assertError
+import com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
+import com.schibsted.spain.barista.interaction.BaristaClickInteractions.clickOn
+import com.schibsted.spain.barista.interaction.BaristaEditTextInteractions.writeTo
+
+class LoginRobot {
+    fun inputEmail(email: String) = apply {
+        writeTo(R.id.emailInputField, email)
+    }
+
+    fun inputPassword(password: String) = apply {
+        writeTo(R.id.passwordInputField, password)
+    }
+
+    fun clickLoginButton() = apply {
+        clickOn(R.id.loginButton)
+    }
+
+    fun assertEmptyEmail() = apply {
+        assertDisplayed(R.id.emailInputField, EMPTY)
+    }
+
+    fun assertEmptyPassword() = apply {
+        assertDisplayed(R.id.passwordInputField, EMPTY)
+    }
+
+    fun assertPasswordError() = apply {
+        assertError(R.id.passwordInputField, R.string.login_password_error)
+    }
+
+    companion object {
+        const val EMPTY = ""
+    }
+}
